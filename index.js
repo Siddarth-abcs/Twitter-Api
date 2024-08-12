1dc58455f7275dc1b4a907dd00fdf5a3e7a3061c
Developer Siddarth <142804820+Siddarth-abcs@users.noreply.github.com>
2024-08-12 18:03:23 +0530

    Razorpay added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+const Razorpay = require("razorpay");
+const crypto = require("crypto");
 
 const app = express();
 
@@ -22,6 +24,7 @@ connectToMongo();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 const postRoutes = require("./routes/postRoutes");
@@ -34,6 +37,31 @@ app.use("/", subscribeRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+// Razorpay
+app.post("/order", async (req, res) => {
+  try {
+    const razorpay = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET,
+    });
+
+    if (!req.body) {
+      return res.status(400).json({ error: "Bad Request" });
+    }
+    const options = req.body;
+
+    const order = await razorpay.orders.create(options);
+
+    if (!order) {
+      return res.status(500).json({ error: "Bad Request" });
+    }
+    res.json(order);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 });
 
 // Start the server
